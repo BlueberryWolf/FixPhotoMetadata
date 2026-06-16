4e246ec36ce233eb5fd16dd74b2979b3660a45d7
BlueberryWolf <blue@blueberry.coffee>
2026-06-15 21:40:36 -0600

    ResoniteScreenshotExtensionsExtensions

--- FixPhotoMetadata.cs
+++ FixPhotoMetadata.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using HarmonyLib;
 using ResoniteModLoader;
 using FrooxEngine;
@@ -14,33 +15,81 @@ namespace FixPhotoMetadata
         public override string Version => "1.0.0";
         public override string Link => "https://github.com/BlueberryWolf/FixPhotoMetadata";
 
+        [AutoRegisterConfigKey]
+        public static readonly ModConfigurationKey<bool> InjectOcclusionKey =
+            new ModConfigurationKey<bool>("InjectOcclusion", "Inject occlusion/in-view data into screenshot metadata", () => true);
+
+        private static ModConfiguration? _config;
+
         public struct CapturedMetadata
         {
             public float3 Position;
             public floatQ Rotation;
             public float FOV;
+            public float Aspect;
 
-            public CapturedMetadata(float3 position, floatQ rotation, float fov)
+            public CapturedMetadata(float3 position, floatQ rotation, float fov, float aspect)
             {
                 Position = position;
                 Rotation = rotation;
                 FOV = fov;
+                Aspect = aspect;
             }
         }
 
         public static readonly Queue<CapturedMetadata> PendingCaptures = new Queue<CapturedMetadata>();
+        public static readonly HashSet<string> OccludedUserIds = new HashSet<string>();
 
         public override void OnEngineInit()
         {
+            _config = GetConfiguration();
             Harmony harmony = new Harmony("com.github.BlueberryWolf.FixPhotoMetadata");
             harmony.PatchAll();
+
+            // try to hook metadata serializer from ResoniteScreenshotExtensions
+            // ie: ScreenshotExtensionsExtensions LOL
+            try
+            {
+                var type = Type.GetType("ResoniteScreenshotExtensions.XmpMetadata, ResoniteScreenshotExtensions");
+                if (type == null)
+                {
+                    foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+                    {
+                        var t = asm.GetType("ResoniteScreenshotExtensions.XmpMetadata");
+                        if (t != null)
+                        {
+                            type = t;
+                            break;
+                        }
+                    }
+                }
+
+                if (type != null)
+                {
+                    var targetMethod = type.GetMethod("SerializeMetadataUserInfo", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                    var postfixMethod = typeof(XmpMetadata_SerializeMetadataUserInfo_Patch).GetMethod("Postfix", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static);
+                    if (targetMethod != null && postfixMethod != null)
+                    {
+                        harmony.Patch(targetMethod, postfix: new HarmonyMethod(postfixMethod));
+                        Msg("Successfully patched ResoniteScreenshotExtensions to inject occlusion/in-view metadata!");
+                    }
+                }
+                else
+                {
+                    Msg("ResoniteScreenshotExtensions not found. Skipping optional XMP metadata injection patch.");
+                }
+            }
+            catch (Exception e)
+            {
+                Error("Failed to patch ResoniteScreenshotExtensions: " + e);
+            }
         }
 
         [HarmonyPatch(typeof(PhotoCaptureManager), "TakePhoto", new Type[] { typeof(Slot), typeof(int2), typeof(bool) })]
         class PhotoCaptureManager_TakePhoto_Patch
         {
             [HarmonyPostfix]
-            static void Postfix(PhotoCaptureManager __instance)
+            static void Postfix(PhotoCaptureManager __instance, int2 resolution)
             {
                 try
                 {
@@ -69,9 +118,10 @@ namespace FixPhotoMetadata
                     }
 
                     float fov = camera.FieldOfView.Value;
+                    float aspect = (resolution.y > 0) ? ((float)resolution.x / resolution.y) : (16f / 9f);
                     lock (PendingCaptures)
                     {
-                        PendingCaptures.Enqueue(new CapturedMetadata(globalPos, globalRot, fov));
+                        PendingCaptures.Enqueue(new CapturedMetadata(globalPos, globalRot, fov, aspect));
                     }
                 }
                 catch (Exception e)
@@ -85,7 +135,7 @@ namespace FixPhotoMetadata
         class InteractiveCamera_Capture_Patch
         {
             [HarmonyPostfix]
-            static void Postfix(InteractiveCamera __instance, InteractiveCamera.Mode mode, bool __result)
+            static void Postfix(InteractiveCamera __instance, InteractiveCamera.Mode mode, int2 resolution, bool __result)
             {
                 if (!__result) return;
 
@@ -97,10 +147,11 @@ namespace FixPhotoMetadata
                     float3 globalPos = mainCamera.Slot.GlobalPosition;
                     floatQ globalRot = mainCamera.Slot.GlobalRotation;
                     float fov = (mode == InteractiveCamera.Mode.Camera360) ? 360f : mainCamera.FieldOfView.Value;
+                    float aspect = (resolution.y > 0) ? ((float)resolution.x / resolution.y) : (16f / 9f);
 
                     lock (PendingCaptures)
                     {
-                        PendingCaptures.Enqueue(new CapturedMetadata(globalPos, globalRot, fov));
+                        PendingCaptures.Enqueue(new CapturedMetadata(globalPos, globalRot, fov, aspect));
                     }
                 }
                 catch (Exception e)
@@ -118,6 +169,11 @@ namespace FixPhotoMetadata
             {
                 try
                 {
+                    lock (OccludedUserIds)
+                    {
+                        OccludedUserIds.Clear();
+                    }
+
                     CapturedMetadata metadata;
                     lock (PendingCaptures)
                     {
@@ -132,10 +188,124 @@ namespace FixPhotoMetadata
                     {
                         photoMetadata.CameraFOV.Value = metadata.FOV;
                     }
+
+                    float aspect = metadata.Aspect;
+                    float fovRad = metadata.FOV * MathF.PI / 180f;
+                    float tanHalfFov = MathF.Tan(fovRad / 2f);
+                    floatQ qInv = new floatQ(-metadata.Rotation.x, -metadata.Rotation.y, -metadata.Rotation.z, metadata.Rotation.w);
+
+                    for (int i = 0; i < __instance.UserInfos.Count; i++)
+                    {
+                        var userInfo = __instance.UserInfos[i];
+                        if (userInfo.User.Target == null) continue;
+
+                        float3 headPos = userInfo.HeadPosition.Value;
+                        float3 relativePos = headPos - metadata.Position;
+                        float3 localPos = qInv * relativePos;
+
+                        bool visible = true;
+
+                        if (localPos.z <= 0.05f)
+                        {
+                            visible = false;
+                        }
+                        else
+                        {
+                            float verticalLimit = localPos.z * tanHalfFov;
+                            float horizontalLimit = verticalLimit * aspect;
+
+                            if (MathF.Abs(localPos.x) > horizontalLimit * 1.2f || MathF.Abs(localPos.y) > verticalLimit * 1.2f)
+                            {
+                                visible = false;
+                            }
+                            else
+                            {
+                                float3 dir = relativePos.Normalized;
+                                float dist = relativePos.Magnitude;
+                                if (dist > 0.3f)
+                                {
+                                    var hit = __instance.World.Physics.RaycastOne(metadata.Position, dir, dist, c => {
+                                        // skip triggers & no-collision slots
+                                        var col = c as Collider;
+                                        if (col != null)
+                                        {
+                                            var type = col.Type.Value;
+                                            if (type == ColliderType.Trigger || type == ColliderType.StaticTrigger || type == ColliderType.NoCollision)
+                                                return false;
+                                        }
+
+                                        // skip target's own avatar
+                                        if (userInfo.User.Target != null && userInfo.User.Target.Root != null && c.Slot.IsChildOf(userInfo.User.Target.Root.Slot))
+                                            return false;
+
+                                        // skip the camera itself
+                                        if (c.Slot.IsChildOf(__instance.Slot))
+                                            return false;
+
+                                        return true;
+                                    });
+
+                                    if (hit.HasValue)
+                                    {
+                                        visible = false;
+                                    }
+                                }
+                            }
+                        }
+
+                        if (!visible)
+                        {
+                            lock (OccludedUserIds)
+                            {
+                                var userId = userInfo.User.Target.UserID;
+                                if (userId != null)
+                                {
+                                    OccludedUserIds.Add(userId);
+                                }
+                            }
+                        }
+                    }
                 }
                 catch (Exception e)
                 {
                     Error("Error in AssetMetadata.SetFromCurrentWorld postfix: " + e);
+                }
+            }
+        }
+
+        class XmpMetadata_SerializeMetadataUserInfo_Patch
+        {
+            internal static void Postfix(XElement element, object userInfo)
+            {
+                if (!(_config?.GetValue(InjectOcclusionKey) ?? true)) return;
+
+                try
+                {
+                    var userProp = userInfo.GetType().GetProperty("User");
+                    if (userProp == null) return;
+                    var userObj = userProp.GetValue(userInfo);
+                    if (userObj == null) return;
+                    var idProp = userObj.GetType().GetProperty("Id");
+                    if (idProp == null) return;
+                    var userId = idProp.GetValue(userObj) as string;
+                    if (userId == null) return;
+
+                    bool isOccluded = false;
+                    lock (OccludedUserIds)
+                    {
+                        isOccluded = OccludedUserIds.Contains(userId);
+                    }
+
+                    var ns = XNamespace.Get("http://ns.baru.dev/resonite-ss-ext/2.0/");
+                    var scaleProp = userInfo.GetType().GetProperty("UserScale");
+                    float userScale = scaleProp != null ? (float)scaleProp.GetValue(userInfo) : 1.0f;
+
+                    element.SetAttributeValue(ns + "UI-HeadScale", userScale.ToString(System.Globalization.CultureInfo.InvariantCulture));
+                    element.SetAttributeValue(ns + "UI-IsInView", (!isOccluded).ToString().ToLower());
+                }
+                catch
+                {
+                    // ignore
                 }
             }
         }
--- README.md
+++ README.md
@@ -5,6 +5,18 @@ A Resonite Mod using Harmony to fix two bugs in the game's photo metadata collec
 1. **Incorrect Camera FOV Timing**: Captures the precise camera FOV at the exact moment a photo is initiated, preventing default/delayed values from being saved if the user breaks the finger gesture before rendering completes.
 2. **Incorrect TakenGlobal Coordinates**: Overwrites `TakenGlobalPosition` and `TakenGlobalRotation` with the actual camera/view coordinates at the moment the capture starts, instead of the unpositioned photo slot's default coordinates (which resulted in `0` or user root coordinates).
 
+## Dynamic Optional Integrations
+
+### GalleVR / ResoniteScreenshotExtensions (aka "ScreenshotExtensionsExtensions" LOL)
+
+If `ResoniteScreenshotExtensions` is also installed/loaded, this mod dynamically patches it at runtime (without static dependencies) to add extra metadata fields:
+
+- **Visibility & Occlusion Check**: Checks if players are in the camera view and runs raycasts to filter out players hidden behind walls/objects (ignoring triggers, camera slots, and self-avatar colliders).
+- **Head Bounding Box Scaling**: Injects `UI-HeadScale` and `UI-IsInView` as separate attributes.
+- **Viewers Compatibility**: Keeping the attributes separate means `UI-HeadPosition` remains a standard `[x; y; z]` value, so it doesn't break other XMP metadata viewers.
+
+This can be disabled in the mod settings under `InjectOcclusion`.
+
 ## Requirements
 
 - [Resonite](https://resonite.com/)
@@ -15,3 +27,4 @@ A Resonite Mod using Harmony to fix two bugs in the game's photo metadata collec
 1. Compile the class library or download the compiled `FixPhotoMetadata.dll`.
 2. Move `FixPhotoMetadata.dll` into your Resonite installation's `rml_mods` folder (typically `C:\Program Files (x86)\Steam\steamapps\common\Resonite\rml_mods`).
 3. Start the game.
+
